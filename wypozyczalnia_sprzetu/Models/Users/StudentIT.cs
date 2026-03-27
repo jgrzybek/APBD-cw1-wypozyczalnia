0c55e002b49aabe0b2615bd0c0990c130b141138
jgrzybek <s29764@pjwstk.edu.pl>
2026-03-27 23:06:40 +0100

    fixed StudentIT Type

--- wypozyczalnia_sprzetu/Models/Users/StudentIT.cs
+++ wypozyczalnia_sprzetu/Models/Users/StudentIT.cs
@@ -4,6 +4,6 @@ namespace wypozyczalnia_sprzetu.Models.Users;
 
 public class StudentIT(string name, string surname, int semester) : User(name, surname, Type)
 {
-    private const UserType Type = UserType.Employee;
+    private const UserType Type = UserType.Student;
     public int Semester { get; set; } = semester;
 }
