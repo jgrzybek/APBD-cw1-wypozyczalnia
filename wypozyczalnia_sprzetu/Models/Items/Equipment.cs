e375cbcae24af2f30dd19653b80f1273a33e9776
jgrzybek <s29764@pjwstk.edu.pl>
2026-03-27 21:26:55 +0100

    added base Equipment class

---
+++ wypozyczalnia_sprzetu/Models/Items/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using wypozyczalnia_sprzetu.Enums;
+
+namespace wypozyczalnia_sprzetu.Models.Items;
+
+public abstract class Equipment(string name, double value, double weightInKilograms)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; set; } =  _nextId++;
+    public string Name { get; set; } = name;
+    public double Value { get; set; } = value;
+    public double Weight { get; set; } = weightInKilograms;
+    public EquipmentStatus Status { get; set; } = EquipmentStatus.Available;
+}
