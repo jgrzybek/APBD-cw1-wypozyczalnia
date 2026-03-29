64e8fbddbbf2454825472b722d897d415ee75d49
jgrzybek <s29764@pjwstk.edu.pl>
2026-03-29 13:09:22 +0200

    Added IRentalService interfce

---
+++ wypozyczalnia_sprzetu/Services/Rentals/IRentalService.cs
@@ -0,0 +1,14 @@
+﻿using wypozyczalnia_sprzetu.Models;
+using wypozyczalnia_sprzetu.Models.Items;
+using wypozyczalnia_sprzetu.Models.Users;
+
+namespace wypozyczalnia_sprzetu.Services.Rentals;
+
+public interface IRentalService
+{
+    public void CreateRental(User user, Equipment equipment, DateTime startDate, DateTime endDate);
+    public void EndRental(int rentalId);
+    public List<Rental> getActiveRentals(int userId);
+    public List<Rental> getOverdueRentals();
+    public List<Tuple<string, int>> getRaport();
+}
