fc2992392670b4fe3cf1714472f6725b958e32d9
jgrzybek <s29764@pjwstk.edu.pl>
2026-03-30 23:14:18 +0200

    added Equipment Service

---
+++ wypozyczalnia_sprzetu/Services/Equipment/IEquipmentService.cs
@@ -0,0 +1,13 @@
+﻿using wypozyczalnia_sprzetu.Enums;
+
+namespace wypozyczalnia_sprzetu.Services.Equipment;
+using wypozyczalnia_sprzetu.Models.Items;
+
+public interface IEquipmentService
+{
+    public void Add(Equipment equipment);
+    public Dictionary<Equipment, EquipmentStatus> GetAllWithStatus();
+    public List<Equipment> GetAvailable();
+    public void SetUnavailable(Equipment equipment);
+    
+}
